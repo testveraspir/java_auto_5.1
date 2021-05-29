d350eeb0e5b12709b4968cfc2ca369a86717c4b9
Vera Spiridonova <ver-spiridonova@yandex.ru>
2021-05-29 17:38:36 +0300

    Заказ доставки карты (изменение даты)

---
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -0,0 +1,49 @@
+package ru.netology.delivery.data;
+
+import com.github.javafaker.Faker;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+
+public class DataGenerator {
+
+
+    private DataGenerator() {
+    }
+
+    public static String generateDate(int shift) {
+
+        LocalDate currentDate = LocalDate.now();
+        currentDate = currentDate.plusDays(shift);
+        String dateFormat = currentDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        String date = String.valueOf(dateFormat);
+        return date;
+    }
+
+    public static String generateCity(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        String city = faker.address().cityName();
+        return city;
+    }
+
+    public static String generateName(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        String name = faker.name().lastName() + " " + faker.name().firstName();
+        return name;
+    }
+
+    public static String generatePhone(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        String phone = faker.phoneNumber().phoneNumber();
+        return phone;
+    }
+
+}
+
+
+
+
+
+
---
+++ src/test/java/ru/netology/delivery/test/CardDeliveryTest.java
@@ -0,0 +1,139 @@
+package ru.netology.delivery.test;
+
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import ru.netology.delivery.data.DataGenerator;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardDeliveryTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+
+    @Test
+    void validFiled() {
+        $("[placeholder='Город']").setValue(DataGenerator.generateCity("ru"));
+        $("[type='tel']").sendKeys(Keys.CONTROL, "a");
+        $("[type='tel']").sendKeys(Keys.DELETE);
+        val daysToAddForFirstMeeting = 4;
+        $("[type='tel']").sendKeys(DataGenerator.generateDate(daysToAddForFirstMeeting));
+        $("[data-test-id='name'] input").setValue(DataGenerator.generateName("ru"));
+        $("[name='phone']").setValue(DataGenerator.generatePhone("ru"));
+        $("[data-test-id='agreement']").click();
+        $(byText("Запланировать")).click();
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(60000));
+        $$(".notification__content").findBy(text("Встреча успешно запланирована на " + DataGenerator.generateDate(daysToAddForFirstMeeting))).shouldBe(visible, Duration.ofSeconds(60000));
+        $("[type='tel']").sendKeys(Keys.CONTROL, "a");
+        $("[type='tel']").sendKeys(Keys.DELETE);
+        val daysToAddForSecondMeeting = 7;
+        $("[type='tel']").sendKeys(DataGenerator.generateDate(daysToAddForSecondMeeting));
+        $$("button").find(exactText("Запланировать")).click();
+        $(withText("У вас уже запланирована встреча на другую дату. Перепланировать?")).shouldBe(visible, Duration.ofSeconds(60000));
+        $$(".button__text").find(text("Перепланировать")).click();
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(60000));
+        $$(".notification__content").findBy(text("Встреча успешно запланирована на " + DataGenerator.generateDate(daysToAddForSecondMeeting))).shouldBe(visible, Duration.ofSeconds(60000));
+    }
+
+    @Test
+    void invalidFiledCity() {
+        $("[placeholder='Город']").setValue("США");
+        $(byText("Запланировать")).click();
+        $("[data-test-id='city']").shouldBe(exactText("Доставка в выбранный город недоступна"));
+    }
+
+    @Test
+    void emptyFiledCity() {
+        $(byText("Запланировать")).click();
+        $("[data-test-id='city']").shouldBe(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void invalidFileDate() {
+        $("[placeholder='Город']").setValue(DataGenerator.generateCity("ru"));
+        $("[type='tel']").sendKeys(Keys.CONTROL, "a");
+        $("[type='tel']").sendKeys(Keys.DELETE);
+        $(byText("Запланировать")).click();
+        $(".calendar-input__custom-control ").shouldBe(exactText("Неверно введена дата"));
+    }
+
+    @Test
+    void invalidFiledName() {
+        $("[placeholder='Город']").setValue(DataGenerator.generateCity("ru"));
+        $("[type='tel']").sendKeys(Keys.CONTROL, "a");
+        $("[type='tel']").sendKeys(Keys.DELETE);
+        val daysToAddForFirstMeeting = 4;
+        $("[type='tel']").sendKeys(DataGenerator.generateDate(daysToAddForFirstMeeting));
+        $("[data-test-id='name'] input").setValue("Petrov Ivan");
+        $(byText("Запланировать")).click();
+        $("[data-test-id='name']").shouldBe(text("Имя и Фамилия указаные неверно."));
+
+    }
+
+    @Test
+    void emptyFiledName() {
+        $("[placeholder='Город']").setValue(DataGenerator.generateCity("ru"));
+        $("[type='tel']").sendKeys(Keys.CONTROL, "a");
+        $("[type='tel']").sendKeys(Keys.DELETE);
+        val daysToAddForFirstMeeting = 4;
+        $("[type='tel']").sendKeys(DataGenerator.generateDate(daysToAddForFirstMeeting));
+        $(byText("Запланировать")).click();
+        $("[data-test-id='name'] .input__sub").shouldBe(exactText("Поле обязательно для заполнения"));
+
+    }
+
+    @Test
+    void emptyFiledNumber() {
+        $("[placeholder='Город']").setValue(DataGenerator.generateCity("ru"));
+        $("[type='tel']").sendKeys(Keys.CONTROL, "a");
+        $("[type='tel']").sendKeys(Keys.DELETE);
+        val daysToAddForFirstMeeting = 4;
+        $("[type='tel']").sendKeys(DataGenerator.generateDate(daysToAddForFirstMeeting));
+        $("[data-test-id='name'] input").setValue(DataGenerator.generateName("ru"));
+        $(byText("Запланировать")).click();
+        $("[data-test-id='phone']").shouldBe(text("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void invalidCheckbox() {
+        $("[placeholder='Город']").setValue(DataGenerator.generateCity("ru"));
+        $("[type='tel']").sendKeys(Keys.CONTROL, "a");
+        $("[type='tel']").sendKeys(Keys.DELETE);
+        val daysToAddForFirstMeeting = 4;
+        $("[type='tel']").sendKeys(DataGenerator.generateDate(daysToAddForFirstMeeting));
+        $("[data-test-id='name'] input").setValue(DataGenerator.generateName("ru"));
+        $("[name='phone']").setValue(DataGenerator.generatePhone("ru"));
+        $(byText("Запланировать")).click();
+        $(".checkbox_checked").shouldBe(hidden);
+    }
+
+    @Test
+    void invalidSecondFiledPhone() {
+        $("[placeholder='Город']").setValue(DataGenerator.generateCity("ru"));
+        $("[type='tel']").sendKeys(Keys.CONTROL, "a");
+        $("[type='tel']").sendKeys(Keys.DELETE);
+        val daysToAddForFirstMeeting = 4;
+        $("[type='tel']").sendKeys(DataGenerator.generateDate(daysToAddForFirstMeeting));
+        $("[data-test-id='name'] input").setValue(DataGenerator.generateName("ru"));
+        $("[name='phone']").setValue(DataGenerator.generatePhone("ru"));
+        $("[data-test-id='agreement']").click();
+        $(byText("Запланировать")).click();
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(60000));
+        $$(".notification__content").findBy(text("Встреча успешно запланирована на " + DataGenerator.generateDate(daysToAddForFirstMeeting))).shouldBe(visible, Duration.ofSeconds(60000));
+        $("[type='tel']").sendKeys(Keys.CONTROL, "a");
+        $("[type='tel']").sendKeys(Keys.DELETE);
+        $$("button").find(exactText("Запланировать")).click();
+        $(".calendar-input__custom-control ").shouldBe(exactText("Неверно введена дата"));
+    }
+
+}
